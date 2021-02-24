91a38ba39c141a8a900c04c185448313f857ab34
johnkorheledu <johnkorhel1@gmail.com>
2021-02-24 11:16:09 -0700

    Move home page

---
+++ Project Source Code/ClassView/src/app/components/logged-in-navbar/logged-in-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logged-in-navbar',
+  templateUrl: './logged-in-navbar.component.html',
+  styleUrls: ['./logged-in-navbar.component.css']
+})
+export class LoggedInNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Project Source Code/ClassView/src/app/components/logged-in/logged-in.component.ts
+++ Project Source Code/ClassView/src/app/components/logged-in/logged-in.component.ts
@@ -1,5 +1,4 @@
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
+import {Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-logged-in',
@@ -7,26 +6,9 @@ import {MediaMatcher} from '@angular/cdk/layout';
   styleUrls: ['./logged-in.component.css']
 })
 export class LoggedInComponent implements OnDestroy {
-  mobileQuery: MediaQueryList;
-
-
-  fillerContent = Array.from({length: 50}, () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
-
-  private _mobileQueryListener: () => void;
-
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
 
   ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
+   
   }
 
 
