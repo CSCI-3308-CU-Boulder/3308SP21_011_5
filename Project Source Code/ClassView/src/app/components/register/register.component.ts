063cfe759510d6e7eb94ed17b00b21c3c78c6334
CarolineCody <carolinechang2097@gmail.com>
2021-02-28 00:38:55 -0800

    email check doesn't work

--- Project Source Code/ClassView/src/app/components/register/register.component.ts
+++ Project Source Code/ClassView/src/app/components/register/register.component.ts
@@ -11,12 +11,87 @@ export class RegisterComponent implements OnInit {
   
   email = new FormControl('', [Validators.required, Validators.email]);
 
+  //Manages the password and password verification status.
+  passGood = false;
+  passVerGood = false;
+  emailGood = false;
+
+  //Stores referential class and text for the password strength bar.
+  passBarClass = ["passLowBar","passMediumBar","passFairBar","passGreatBar"];
+  passLabelClass = ["passLowLabel","passMediumLabel","passFairLabel","passGreatLabel"];
+  passLabelWords = ["Poor","Medium","Fair","Great"];
+  
+  //Local copy of the passwords.
+  password: string;
+  verPassword: string;
+
   getErrorMessage() {
     if (this.email.hasError('required')) {
+      this.emailGood = false;
+      this.activateTurnIn();
       return 'You must enter a value';
     }
+    else if(this.email.hasError('email')){
+      this.emailGood = false;
+      this.activateTurnIn();
+      return 'Not a valid email';
+    }
+    else{
+      this.emailGood = true;
+      this.activateTurnIn();
+      return '';
+    }
+  }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+  //Checks to see if the password is up to spec. Being of a certain length
+ checkPassword(input){
+    this.password = (<HTMLInputElement>document.getElementById(input)).value;
+    let counter = 0;
+    let checklist = {  
+      digits: /\d/.test(this.password),  
+      lower: /[a-z]/.test(this.password),  
+      upper: /[A-Z]/.test(this.password),   
+      nonWords: /\W/.test(this.password),  
+    };  
+    if(this.password.length >= 8){
+      counter++;
+    }
+    for (let check in checklist){
+      counter += (checklist[check]) ? 1 : 0; 
+    }
+    counter = Math.max(counter-2,0);
+    let bar = document.getElementById("strengthBar");
+    let lab = document.getElementById("strengthLabel");
+    bar.classList.remove("hidden","passLowBar","passMediumBar","passFairBar","passGreatBar");
+    lab.classList.remove("hidden","passLowLabel","passMediumLabel","passFairLabel","passGreatLabel");
+    bar.classList.add(this.passBarClass[counter]);
+    lab.innerHTML = this.passLabelWords[counter];
+    lab.classList.add(this.passLabelClass[counter]);
+    if(counter >= 3){
+      this.passGood = true;
+    }
+    else{
+      this.passGood = false;
+    }
+    this.activateTurnIn();
+  }
+
+  confirmPassword(input){
+    this.verPassword = input.value;
+    this.passVerGood = (this.verPassword == this.password);
+    this.activateTurnIn();
+  }
+
+  //Handles activation and diactivation of the register button.
+  activateTurnIn(){
+    if(this.emailGood && this.passGood && this.passVerGood){
+      document.getElementById('registerButton').classList.add("found");
+      document.getElementById('registerButton').classList.remove("hidden");
+    }
+    else{
+      document.getElementById('registerButton').classList.add("hidden");
+      document.getElementById('registerButton').classList.remove("found");
+    }
   }
 
   constructor() { }
