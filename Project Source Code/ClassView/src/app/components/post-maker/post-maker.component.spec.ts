72d958b46a6f0ddc1f7ad75b7a9541dfdea67060
CarolineCody <carolinechang2097@gmail.com>
2021-03-06 20:55:45 -0800

    Well done C!

---
+++ Project Source Code/ClassView/src/app/components/post-maker/post-maker.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { PostMakerComponent } from './post-maker.component';
+
+describe('PostMakerComponent', () => {
+  let component: PostMakerComponent;
+  let fixture: ComponentFixture<PostMakerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PostMakerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PostMakerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
