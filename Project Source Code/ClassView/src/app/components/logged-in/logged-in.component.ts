87bafb3711930ebb6c2b4a90adebfa5db82ba400
CarolineCody <carolinechang2097@gmail.com>
2021-03-07 22:17:06 -0800

    response creation may have to go to a seperate page smh.

--- Project Source Code/ClassView/src/app/components/logged-in/logged-in.component.ts
+++ Project Source Code/ClassView/src/app/components/logged-in/logged-in.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnDestroy } from '@angular/core';
+import { ToolbarService, LinkService, ImageService, HtmlEditorService, TableService } from '@syncfusion/ej2-angular-richtexteditor';
 
 @Component({
   selector: 'app-logged-in',
-  templateUrl: './logged-in.component.html',
+  templateUrl: './logged-in.component.html',    providers: [ToolbarService, LinkService, ImageService, HtmlEditorService, TableService],
   styleUrls: ['./logged-in.component.css']
 })
 export class LoggedInComponent implements OnDestroy {
@@ -10,6 +11,15 @@ export class LoggedInComponent implements OnDestroy {
    
   }
 
+  public tools: object = {
+    items: [
+      'Bold', 'Italic', 'Underline', 'StrikeThrough', '|',
+      'FontName', 'FontSize', 'FontColor', 'BackgroundColor', '|',
+      'LowerCase', 'UpperCase', '|', 'Undo', 'Redo', '|',
+      'Formats', 'Alignments', '|', 'OrderedList', 'UnorderedList', '|',
+      'Indent', 'Outdent', '|', 'CreateLink','CreateTable',
+      'Image', '|', 'ClearFormat', 'Print', 'SourceCode', '|', 'FullScreen']
+  };
   
   upVote(id){
     var voteCount = parseInt((<HTMLInputElement>document.getElementById("postVoteLabel_"+id)).innerHTML,10);
